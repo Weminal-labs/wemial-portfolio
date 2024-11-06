5d8d94183ff9f4a6a0dec62c57bb3fcbd07ecaa8
Huc06 <phuc92147@gmail.com>
2024-11-06 11:26:12 +0700

    fix Image

--- src/components/Members.tsx
+++ src/components/Members.tsx
@@ -13,7 +13,7 @@ const Members = () => {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <h2 className="text-4xl font-bold text-center mb-4 tracking-wide">OUR DEDICATED TEAM</h2>
           <p className="text-center text-gray-700 mb-12 max-w-2xl mx-auto leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br /> Nullam aliquet, nisi sit amet ultrices tempor, odio <br /> magna tempus libero, in volutpat odio turpis sit amet <br /> urna. Integer a ex nec risus scelerisque dapibus. Nam <br /> cursus metus vel lorem varius, a laculis ligula sodales.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br /> Nullam aliquet, nisi sit amet ultrices tempor, odio <br /> magna tempus libero, in volutpat odio turpis sit amet <br /> urna. Integer a ex nec risus scelerisque dapibus. Nam <br />cursus metus vel lorem varius, a laculis ligula sodales.
           </p>
         <Swiper
           modules={[Autoplay]}
@@ -27,7 +27,7 @@ const Members = () => {
               <Image
                 src="https://harryph.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fdf0f4344-aa59-48dc-938b-a5736604ea95%2F1cbddb9f-7a98-486e-b0b0-754b1926f108%2Fmy_profile.png?id=1e47796a-428b-4252-850e-ffcc85e62db6&table=block&spaceId=df0f4344-aa59-48dc-938b-a5736604ea95&width=250&userId=&cache=v2" 
                 alt="Member 9"
-                className="w-full h-48 object-cover"
+                className="w-full h-48 object-contain"
                 width={300}
                 height={300}
               />
@@ -42,7 +42,7 @@ const Members = () => {
               <Image
                 src="https://harryph.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fdf0f4344-aa59-48dc-938b-a5736604ea95%2F521be69c-46d2-4e71-b3ec-c3d041327e1d%2Fnotion-avatar-1723024343582.png?id=3204c494-f43d-424c-baa0-568928692ba1&table=block&spaceId=df0f4344-aa59-48dc-938b-a5736604ea95&width=250&userId=&cache=v2" 
                 alt="Member 10"
-                className="w-full h-48 object-cover"
+                className="w-full h-48 object-contain"
                 width={300}
                 height={300}
               />
@@ -57,7 +57,7 @@ const Members = () => {
               <Image
                 src="https://harryph.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fdf0f4344-aa59-48dc-938b-a5736604ea95%2F317f5e16-74a7-4a31-b5a3-fe6c832c8ac1%2Fphapdev-notion-avatar.png?id=435cedd9-7489-4b28-88a3-19683e7e0910&table=block&spaceId=df0f4344-aa59-48dc-938b-a5736604ea95&width=250&userId=&cache=v2" 
                 alt="Member 1"
-                className="w-full h-48 object-cover"
+                className="w-full h-48 object-contain"
                 width={300}
                 height={300}
               />
@@ -72,7 +72,7 @@ const Members = () => {
               <Image
                 src="https://harryph.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fdf0f4344-aa59-48dc-938b-a5736604ea95%2F609a5543-e9f4-45dc-be44-c0b6d3899546%2Fnotion-avatar-1723018881419.png?id=67912e02-e0e2-4411-bca8-6959a08e0931&table=block&spaceId=df0f4344-aa59-48dc-938b-a5736604ea95&width=250&userId=&cache=v2" 
                 alt="Member 2"
-                className="w-full h-48 object-cover"
+                className="w-full h-48 object-contain"
                 width={300}
                 height={300}
               />
@@ -87,7 +87,7 @@ const Members = () => {
               <Image
                 src="https://harryph.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fdf0f4344-aa59-48dc-938b-a5736604ea95%2F94f152fc-e757-4a76-83ea-1b8c93e82a94%2Favt_dorahck.png?id=fa9c46e8-6d8d-4a11-8ab0-2ec8d98df65a&table=block&spaceId=df0f4344-aa59-48dc-938b-a5736604ea95&width=250&userId=&cache=v2" 
                 alt="Member 3"
-                className="w-full h-48 object-cover"
+                className="w-full h-48 object-contain"
                 width={300}
                 height={300}
               />
@@ -102,7 +102,7 @@ const Members = () => {
               <Image
                 src="https://harryph.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fdf0f4344-aa59-48dc-938b-a5736604ea95%2F1ce9546f-f677-4458-8bdc-7f5410211039%2Favt-notion.png?table=block&id=0c6e6409-80d2-4b47-b11f-add34f999bc2&spaceId=df0f4344-aa59-48dc-938b-a5736604ea95&width=720&userId=&cache=v2" 
                 alt="Member 4"
-                className="w-full h-48 object-cover"
+                className="w-full h-48 object-contain"
                 width={300}
                 height={300}
               />
@@ -117,7 +117,7 @@ const Members = () => {
               <Image
                 src="https://harryph.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fdf0f4344-aa59-48dc-938b-a5736604ea95%2F05c1ea40-ab73-4a95-946f-29dfee9803ba%2Fimage.png?table=block&id=d0bcb269-eabe-44db-8103-26ee85657215&spaceId=df0f4344-aa59-48dc-938b-a5736604ea95&width=2000&userId=&cache=v2" 
                 alt="Member 5"
-                className="w-full h-48 object-cover"
+                className="w-full h-48 object-contain"
                 width={300}
                 height={300}
               />
@@ -132,7 +132,7 @@ const Members = () => {
               <Image
                 src="https://harryph.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fdf0f4344-aa59-48dc-938b-a5736604ea95%2F12c74d5b-13cf-45e6-9a21-e250d3c0103a%2FUntitled.png?table=block&id=f534c4e5-85a5-4e38-99bb-009aca6281e5&spaceId=df0f4344-aa59-48dc-938b-a5736604ea95&width=2000&userId=&cache=v2" 
                 alt="Member 6"
-                className="w-full h-48 object-cover"
+                className="w-full h-48 object-contain"
                 width={300}
                 height={300}
               />
@@ -147,7 +147,7 @@ const Members = () => {
               <Image
                 src="https://harryph.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fdf0f4344-aa59-48dc-938b-a5736604ea95%2Fe3fed5bd-f2b3-49ba-b3ad-b96f8f2b23f1%2Fnotion-avatar-1723093607415.png?table=block&id=2200c3b7-ac34-4402-88c3-ed83f09ef07b&spaceId=df0f4344-aa59-48dc-938b-a5736604ea95&width=2000&userId=&cache=v2" 
                 alt="Member 7"
-                className="w-full h-48 object-cover"
+                className="w-full h-48 object-contain"
                 width={300}
                 height={300}
               />
@@ -162,7 +162,7 @@ const Members = () => {
               <Image
                 src="https://harryph.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fdf0f4344-aa59-48dc-938b-a5736604ea95%2Fd22e6042-1ace-465a-bfc6-c11ad1f12959%2Fnotion-avatar-1723024894283.png?table=block&id=b5b3a7bd-594f-4234-83e5-d4faa0bf5e7a&spaceId=df0f4344-aa59-48dc-938b-a5736604ea95&width=580&userId=&cache=v2" 
                 alt="Member 8"
-                className="w-full h-48 object-cover"
+                className="w-full h-48 object-contain"
                 width={300}
                 height={300}
               />
@@ -177,7 +177,7 @@ const Members = () => {
               <Image
                 src="https://harryph.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fdf0f4344-aa59-48dc-938b-a5736604ea95%2F9f43e807-f91f-4f70-936b-8d8b456b9301%2Fnotion-avatar-1723024795101.png?table=block&id=599f3a08-d670-481a-9b6f-bc58d09c0ed3&spaceId=df0f4344-aa59-48dc-938b-a5736604ea95&width=2000&userId=&cache=v2" 
                 alt="Member 11"
-                className="w-full h-48 object-cover"
+                className="w-full h-48 object-contain"
                 width={300}
                 height={300}
               />
@@ -192,7 +192,7 @@ const Members = () => {
               <Image
                 src="https://harryph.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fdf0f4344-aa59-48dc-938b-a5736604ea95%2Fce73084b-ca4a-47e4-bc73-b7d4d474cb06%2FUntitled.png?table=block&id=43bcd81b-1b34-4364-a8db-9d2d9c163810&spaceId=df0f4344-aa59-48dc-938b-a5736604ea95&width=2000&userId=&cache=v2" 
                 alt="Member 12"
-                className="w-full h-48 object-cover"
+                className="w-full h-48 object-contain"
                 width={300}
                 height={300}
               />
@@ -207,7 +207,7 @@ const Members = () => {
               <Image
                 src="https://harryph.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fdf0f4344-aa59-48dc-938b-a5736604ea95%2F89eb4513-9846-406e-92ab-0ed7293f4de8%2Fnotion-avatar-1723038466786.png?id=c81dda49-e151-4099-9c4a-4d67deef40db&table=block&spaceId=df0f4344-aa59-48dc-938b-a5736604ea95&width=250&userId=&cache=v2" 
                 alt="Member 13"
-                className="w-full h-48 object-cover"
+                className="w-full h-48 object-contain"
                 width={300}
                 height={300}
               />
